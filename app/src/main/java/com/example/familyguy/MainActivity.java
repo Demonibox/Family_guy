b71198f9dde0f29c5b9d164a2bd4087201934720
Demonibox <demonibox@gmail.com>
2020-11-17 00:26:54 +0300

    erase_lois

--- app/src/main/java/com/example/familyguy/MainActivity.java
+++ app/src/main/java/com/example/familyguy/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.familyguy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void erasePeter(View view) {
+        ImageView peterImageView = findViewById(R.id.peterimageView);
+        peterImageView.animate().alpha(0).setDuration(3000);
+
+        ImageView loisImageView = findViewById(R.id.loisimageView);
+        loisImageView.animate().alpha(1).setDuration(3000);
+    }
+
+
+    }
